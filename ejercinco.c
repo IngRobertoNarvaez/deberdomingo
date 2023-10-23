6fb0875182ee114a56a601b3335e330708e7b2b6
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 23:37:16 -0500

    Se inicia el segundo for igual con conteo

--- ejercinco.c
+++ ejercinco.c
@@ -7,7 +7,9 @@ int main()
     {
         // Se define que comienza del 1 hasta el 10
         printf("Tabla de multiplicar del %d:\n", i);
-
+        for (j = 1; j <= 10; j++)
+        {
+        }
         printf("\n");
     }
     return 0;
