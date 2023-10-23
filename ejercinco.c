3f2b0c206ea784d301ab00efdc94a532fd4f3c17
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 23:36:26 -0500

    Aparece lo que se mostatra

--- ejercinco.c
+++ ejercinco.c
@@ -5,6 +5,10 @@ int main()
     int i, j, resultado;
     for (i = 1; i <= 10; i++)
     {
+        // Se define que comienza del 1 hasta el 10
+        printf("Tabla de multiplicar del %d:\n", i);
+
+        printf("\n");
     }
     return 0;
 }
