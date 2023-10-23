dc4ecd523f3e61aabf6d34e597d3a1fab4ad4409
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 22:57:04 -0500

    Se inicia el if en el ejercicio

--- ejertres.c
+++ ejertres.c
@@ -5,6 +5,11 @@ int main()
     int n;
     printf("Ingrese un numero entero: "); // Se solicita al usuario que ingrese un numero entero
     scanf("%d", &n);                      // Se lee el numero entero ingresado por el usuario
+    if (n <= 1)
+    { // Si el numero es menor o igual a 1, entonces no es primo
+        printf("%d no es primo.\n", n);
+        return 0;
+    }
 
     return 0;
 }
