d8cfe1c44f6c5fa27bcb6237d8fac7809e8d75d7
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 22:55:05 -0500

    Se define el comienzo

--- ejertres.c
+++ ejertres.c
@@ -2,6 +2,9 @@
 
 int main()
 {
+    int n;
+    printf("Ingrese un numero entero: "); // Se solicita al usuario que ingrese un numero entero
+    scanf("%d", &n);                      // Se lee el numero entero ingresado por el usuario
 
     return 0;
 }
