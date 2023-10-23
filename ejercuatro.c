47209924a6c2c3863d7766cb14bb777d0459601f
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 23:18:07 -0500

    Inicio de los While

--- ejercuatro.c
+++ ejercuatro.c
@@ -22,6 +22,33 @@ int main()
     int num1, num2, digito, contador1 = 0, contador2 = 0;
     printf("Ingrese dos numeros enteros: ");
     scanf("%d %d", &num1, &num2);
+    // Determinar la cantidad de digitos primos en el primer numero
+    while (num1 > 0)
+    {
+        digito = num1 % 10;
+        for (int i = 2; i <= digito; i++)
+        {
+            if (digito % i == 0 && es_primo(i))
+            {
+                contador1++;
+            }
+        }
+        num1 /= 10;
+    }
 
-       return 0;
+    // Determinar la cantidad de digitos primos en el segundo numero
+    while (num2 > 0)
+    {
+        digito = num2 % 10;
+        for (int i = 2; i <= digito; i++)
+        {
+            if (digito % i == 0 && es_primo(i))
+            {
+                contador2++;
+            }
+        }
+        num2 /= 10;
+    }
+
+    return 0;
 }
