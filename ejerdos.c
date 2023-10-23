28d7fa7da9e6f4061339d8326eab380d6280c35c
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 21:35:35 -0500

    Se arma el codigo para que sume

--- ejerdos.c
+++ ejerdos.c
@@ -4,9 +4,14 @@ int main()
 {
     // Se inicia las variables
     int n, sum = 0;
-    printf("Ingrese un número entero: ");
+    printf("Ingrese un numero entero: ");
     // hacemos que lea las variables
     scanf("%d", &n);
+    // se inicia el for para el trabajo de suma
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
 
     return 0;
 }
