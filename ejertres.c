fd0966d4bb870a9f8e388b06da4d555a043e29b0
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 22:57:55 -0500

    Se inicia el for para el ejercicio

--- ejertres.c
+++ ejertres.c
@@ -10,6 +10,17 @@ int main()
         printf("%d no es primo.\n", n);
         return 0;
     }
+    for (int i = 2; i * i <= n; i++)
+    { // Si el numero es mayor que 1, el bucle comienza en 2 y
+        // continua hasta la raiz cuadrada del numero ingresado, ya que ningun factor de un numero
+        // puede ser mayor que su raiz cuadrada
+        if (n % i == 0)
+        { // Si el numero es divisible por algun numero entre 2 y su raiz cuadrada,
+            // entonces no es primo y el programa imprime que el numero no es primo
+            printf("%d no es primo.\n", n);
+            return 0;
+        }
+    }
 
     return 0;
 }
