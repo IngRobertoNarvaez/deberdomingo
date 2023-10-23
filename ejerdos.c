45c74ac86e166e257c5d68ff4679b64a99cffcd7
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 21:33:30 -0500

    Se inicia y varaibles para que las lea

--- ejerdos.c
+++ ejerdos.c
@@ -2,6 +2,11 @@
 // Se arma la estrucura base
 int main()
 {
+    // Se inicia las variables
+    int n, sum = 0;
+    printf("Ingrese un número entero: ");
+    // hacemos que lea las variables
+    scanf("%d", &n);
 
     return 0;
 }
