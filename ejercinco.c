be3ccb4327c909280c1d7b7ea88b8219d31439ae
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 23:38:04 -0500

    Se imprime y final del programa

--- ejercinco.c
+++ ejercinco.c
@@ -9,6 +9,10 @@ int main()
         printf("Tabla de multiplicar del %d:\n", i);
         for (j = 1; j <= 10; j++)
         {
+            // se realiza una multiplicacion del numero nuemo
+            resultado = i * j;
+            printf("%d x %d = %d\n", i, j, resultado);
+            // Se imprime el resultado final
         }
         printf("\n");
     }
