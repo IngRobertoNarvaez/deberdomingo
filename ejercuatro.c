6e55fa0246c926de2a96eed31f6fa17c99e58d1b
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-22 23:15:42 -0500

    Segundo main de la estructura

--- ejercuatro.c
+++ ejercuatro.c
@@ -19,6 +19,9 @@ int es_primo(int n)
 
 int main()
 {
+    int num1, num2, digito, contador1 = 0, contador2 = 0;
+    printf("Ingrese dos numeros enteros: ");
+    scanf("%d %d", &num1, &num2);
 
-    return 0;
+       return 0;
 }
